de59d51bdf80416a18c8e4140e04c7bcbe7cd2c9
Argha Paul <argha.paul@walmartlabs.com>
2018-01-25 16:16:37 +0530

    browser forward and backward navigation added and clear pop state while unmounting

--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,28 @@ const pushState = (obj, url) => {
   window.history.pushState(obj, '', url);
 };
 
+const onPopState = handler => {
+  window.onpopstate = handler; 
+};
+
 class App extends React.Component {
   static propTypes = {
     initialData: PropTypes.object.isRequired
   };
 
-  state = this.props.initialData
+  state = this.props.initialData;
+
+  componentDidMount() {
+    onPopState((event) => {
+      this.setState({
+        currentContestId: (event.state || {}).currentContestId
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    onPopState(null);
+  }
 
   fetchContest = (contestId) => {
     pushState(

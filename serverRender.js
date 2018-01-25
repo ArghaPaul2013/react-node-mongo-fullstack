e90831ae748cc4535307a8b366da6106bf5df85d
Argha Paul <argha.paul@walmartlabs.com>
2018-01-25 15:57:03 +0530

    server side routing handled

--- serverRender.js
+++ serverRender.js
@@ -6,14 +6,39 @@ import App from './src/components/App';
 import config from './config';
 import axios from 'axios';
 
-const serverRender = () =>
-  axios.get(`${config.serverUrl}/api/contests`)
+const getApiUrl = contestId => {
+  if (contestId) {
+    return `${config.serverUrl}/api/contests/${contestId}`;
+  }
+
+  return `${config.serverUrl}/api/contests`;
+};
+
+const getInitialData = (contestId, apiData) => {
+  if (contestId) {
+    return {
+      currentContestId: apiData.id,
+      contests: {
+        [apiData.id]: apiData
+      }
+    };
+  }
+
+  return {
+    contests: apiData.contests
+  };
+};
+
+const serverRender = (contestId) =>
+  axios.get(getApiUrl(contestId))
     .then(response => {
+      const initialData = getInitialData(contestId, response.data);
+
       return {
         initialMarkup: ReactDOMServer.renderToString(
-          <App initialData={response.data} />
+          <App initialData={initialData} />
         ),
-        initialData: response.data
+        initialData
       };
     });
 
--- src/components/App.js
+++ src/components/App.js
@@ -33,13 +33,27 @@ class App extends React.Component {
     });
   }
 
+  fetchContestList = () => {
+    pushState(
+      {currentContestId: null},
+      '/'
+    );
+
+    api.fetchContestList().then(contests => {
+      this.setState({
+        currentContestId: null,
+        contests
+      }); 
+    });
+  }
+
   currentContest() {
     return this.state.contests[this.state.currentContestId];
   }
 
   pageHeader() {
     if (this.state.currentContestId) {
-      return this.currentContest().categoryName;
+      return this.currentContest().contestName;
     }
 
     return 'Naming Contest';
@@ -48,7 +62,9 @@ class App extends React.Component {
   currentContent() {
     if (this.state.currentContestId) {
       return (
-        <Contest {...this.currentContest()} />
+        <Contest
+          contestListClick={this.fetchContestList}
+          {...this.currentContest()} />
       );
     }
 

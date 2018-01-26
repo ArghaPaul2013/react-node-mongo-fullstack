25300fed78bca3c4d661eb20cc3d9e5f8f9d635c
Argha Paul <argha.paul@walmartlabs.com>
2018-01-26 21:09:01 +0530

    hard coded in memory data removed and mongodb integration done

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 const env = process.env;
 
 export const nodeEnv = env.NODE_ENV || 'development';
+export const mongodbName = 'admin';
 export default {
+  mongodbName,
+  mongodbUri: `mongodb://localhost:27017/${mongodbName}`,
   port: env.PORT || 8080,
   host: env.HOST || '0.0.0.0',
   get serverUrl() {
